6fb2484e35ca503a7e6b5b4b88041244fd67460a
Antonela Borgogno <antonelaborgogno@gmail.com>
2026-04-23 15:34:50 -0300

    feat: agregar esquemas Zod para usuario, juego y partida en el paquete compartido

---
+++ packages/shared/index.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+
+// --- USUARIO ---
+export const UserZodSchema = z.object({
+	username: z.string().min(3, 'Mínimo 3 caracteres'),
+	email: z.email('Email inválido'),
+	password: z.string().min(6, 'Mínimo 6 caracteres'),
+	avatar: z.string().optional(),
+});
+
+export type UserDTO = z.infer<typeof UserZodSchema>;
+
+// --- JUEGO (Configuración) ---
+export const GameZodSchema = z.object({
+	name: z.string(),
+	category: z.enum(['Cartas', 'Dados', 'Otros']),
+	minPlayers: z.number().min(2),
+	maxPoints: z.number().optional(), // Depende de cada juego
+});
+
+// --- PARTIDA (Match) ---
+export const MatchPlayerSchema = z.object({
+	userId: z.string().optional(), //} Para invitados invitados
+	name: z.string(),
+	position: z.number(),
+	team: z.enum(['A', 'B', 'None']).default('None'),
+	currentScore: z.number().default(0),
+	isOut: z.boolean().default(false),
+});
+
+export const RoundSchema = z.object({
+	roundNumber: z.number(),
+	dealerIndex: z.number(),
+	scores: z.array(
+		z.object({
+			playerId: z.string(),
+			points: z.number(),
+			details: z.any(), // Aquí se guardan datos especificos de los juegos. ( Mosca, Escoba)
+		}),
+	),
+});
+
+export const MatchZodSchema = z.object({
+	gameId: z.string(),
+	players: z.array(MatchPlayerSchema),
+	status: z.enum(['active', 'finished']).default('active'),
+	dealerIndex: z.number().default(0),
+});
