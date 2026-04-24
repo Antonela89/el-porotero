1cfee9873008180e7410fbd2c4b5e9f85a20365a
Antonela Borgogno <antonelaborgogno@gmail.com>
2026-04-24 15:18:08 -0300

    feat: agrega lógica para obtener detalles de partidas y actualizar el estado de las mismas

--- apps/api/src/routes/match.routes.ts
+++ apps/api/src/routes/match.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
-import { createMatch, addRound } from '@/controllers/match.controller.js';
+import {
+	createMatch,
+	addRound,
+    updateRound,
+	getUserMatches,
+	getMatchById,
+	deleteMatch,
+    updateMatchStatus,
+} from '@/controllers/index.js';
 import { protect } from '@/middlewares/index.js';
 
 const matchRouter = Router();
 
 matchRouter.use(protect); // Todas las rutas de acá abajo requieren Token
 
-matchRouter.post('/', createMatch);
-matchRouter.post('/:matchId/round', addRound);
+matchRouter.get('/', getUserMatches); // GET /api/matches
+matchRouter.get('/:matchId', getMatchById); // GET /api/matches/:id
+matchRouter.put('/:matchId',  updateMatchStatus)
+matchRouter.post('/', createMatch); // POST /api/matches
+matchRouter.post('/:matchId/round', addRound); // POST /api/matches/:id/round
+matchRouter.patch('/:matchId/rounds/:roundNumber', updateRound); //PATCH  /api/matches/:id/round/:id
+matchRouter.delete('/:matchId', deleteMatch); // DELETE /api/matches/:id
 
 export default matchRouter;
