d1edcb6ef5180c395a9482285859df8f3c83b918
Antonela Borgogno <antonelaborgogno@gmail.com>
2026-04-24 12:41:14 -0300

    feat: agrega ruta para manejar partidas en la API

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import authRouter from './routes/auth.routes.js';
+import matchRouter from './routes/match.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 
 // Rutas
 app.use('/api/auth', authRouter);
+app.use('/api/matches', matchRouter);
 
 app.listen(PORT, () => {
 	console.log(`Servidor corriendo en http://localhost:${PORT}`);
